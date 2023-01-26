28befde11fbe94296f9b6647bcc8c71fdb926ae4
Dasian <davidrespiritu99@gmail.com>
2023-01-26 16:08:54 -0500

    Implemented dynamic bfs animation

--- graphs/scenes.py
+++ graphs/scenes.py
@@ -2,25 +2,43 @@ from manim import *
 
 # manim -pql scenes.py BFS
 import generator
+import algos
+from config import * 
 
 class BFS(Scene):
 	def construct(self):
-		# generate a graph
-		n = 5
-		V, E = generator.random_planar_graph(n, p=0.3)
-		graph = Graph(V, E, labels=True, layout="random", layout_scale=4)	
+		
+		# generated graph settings
+		n = 5	# number of nodes
+		p = .3	# probability of removing an edge
+		
+		# manim graph settings
+		labels = True
+		layout = "random"
+		layout_scale = 4
 
-		# create all animation states
-		# init all node and edge colors
+		generated_graph = generator.random_planar_graph(n, p)
+		V = generated_graph.all_vertices()
+		E = generated_graph.all_edges()
+
+		manim_graph = Graph(V, E, labels=labels, layout=layout, layout_scale=layout_scale)	
+
+		# init all manim node and edge colors
 		curr_vc = {}
 		curr_ec = {}
 		for v in V:
-			curr_vc[v] = {"fill_color": GREY}
+			curr_vc[v] = {"fill_color": state_colors[GraphState.DEFAULT]}
 		for e in E:
-			curr_ec[e] = {"stroke_color": GREY}	
-		# generate an ordered list of state change tuples
-		render_queue = []
-		states = algos.bfs_states(V, E)
+			curr_ec[e] = {"stroke_color": state_colors[GraphState.DEFAULT]}	
+		curr_vc[generated_graph.get_start()] = {"fill_color": state_colors[GraphState.START]}
+		curr_vc[generated_graph.get_end()] = {"fill_color": state_colors[GraphState.END]}
+
+		# generate an ordered list of animation state change tuples
+		states = algos.bfs_states(generated_graph)
+
+		self.play(Create(manim_graph), run_time=2)
+
+		# animate the algorithm steps
 		for s in states:
 			vc, ec = s
 
@@ -30,16 +48,13 @@ class BFS(Scene):
 			for k in ec.keys():
 				curr_ec[k] = ec[k]			
 
-			# maybe actually render here? doesn't need to take up space
-			render_queue.append(Graph(V, E, labels=labels, layout=layout, layout_scale=layout_scale, vertex_config=curr_vc, edge_config=curr_ec)
+			# render an animation state
+			# TODO get the right transition funcs
+			next_graph = Graph(V, E, labels=labels, layout=layout, layout_scale=layout_scale, vertex_config=curr_vc, edge_config=curr_ec)
+			self.play(TransformMatchingShapes(manim_graph, next_graph))
+			manim_graph = next_graph
 
-		# render all animation states
-		# (possibly render each state when immediately generated?)
-		for r in render_queue:
-			self.play(r)
-		self.play(Create(graph), run_time=4)
-		self.wait(2)
-		self.play(Uncreate(graph), run_time=4)
+		self.play(Uncreate(manim_graph), run_time=2)
 		self.wait(2)
 
 	# possibly implement tear_down() method
